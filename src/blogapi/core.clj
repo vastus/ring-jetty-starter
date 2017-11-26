f58567dfb1adb8414ea7f5646ce5b8d4620e9e89
Juho Hautala <juho.hautala@helsinki.fi>
2017-11-26 13:39:33 +0200

    Define server w/ a handler

--- src/blogapi/core.clj
+++ src/blogapi/core.clj
@@ -1,5 +1,16 @@
 (ns blogapi.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.adapter.jetty :refer [run-jetty]]))
+
+(defn handler [request]
+  {:status 200
+   :headers {"content-type" "text/plain"}
+   :body "suh?"})
+
+(defonce server
+  (delay
+    (run-jetty #'handler {:port 3000
+                          :join? false})))
 
 (defn -main
   "I don't do a whole lot ... yet."
